f330614909a3a551d26571b8f05ba145d5aacf56
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-02 16:36:30 -0300

    feat: add slot-scope

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-cli-advanced
- Projeto em VueJS, aprendendo conceitos avançados sobre Components 
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="container">
+    <h1>VueJS</h1>
+    
+    <button @click="componentSelecionado = 'HomeComponent'">Home</button>
+    <button @click="componentSelecionado = 'PostsLista'">Lista</button>
+    <button @click="componentSelecionado = 'SobreComponent'">Sobre</button>
+
+    <component :is="componentSelecionado"></component>
+  </div>
+</template>
+
+<script>
+
+import HomeComponent from './components/HomeComponent.vue'
+import PostsLista from './components/PostsLista.vue'
+import SobreComponent from './components/SobreComponent.vue'
+
+export default {
+  components: {
+    HomeComponent,
+    PostsLista,
+    SobreComponent
+  },
+  data () {
+    return {
+        componentSelecionado: 'HomeComponent',
+        posts: [
+            { id: 1, titulo: 'Components no VueJS', conteudo: 'Components são uma das peças mais importantes no VueJS', autor: 'Hesnan Ávila'},
+            { id: 2, titulo: 'Components no VueJS 2', conteudo: 'Components são uma das peças mais importantes no VueJS 2', autor: 'Hesnan Ávila 2'}
+        ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    width: 960px;
+    margin: auto;
+  }
+</style>
